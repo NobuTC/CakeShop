5b0f89d8384104ce57ef0b5de676422b460cdfbe
Jenni Tran <jenni90232@gmail.com>
2023-10-24 23:29:23 +0300

    add add method

--- src/app/api/product/add/route.js
+++ src/app/api/product/add/route.js
@@ -1,3 +1,29 @@
-export async function GET(request) {
-  return Response.json({ message: "add " });
+import prisma from "../../../../../lib/prisma";
+
+export async function POST(request) {
+  const { name, image_url, size, info_allergic, price, category, info } =
+    await request.json();
+
+  const productData = {
+    name,
+    image_url,
+    size,
+    info_allergic,
+    price,
+    category,
+    info,
+  };
+
+  try {
+    const newProduct = await prisma.product.create({
+      data: productData,
+    });
+    console.log("created product!", newProduct);
+    return Response.json({ newProduct });
+  } catch (error) {
+    console.error("Error creating product:", error);
+    return new Response("Cannot add product", {
+      status: 400,
+    });
+  }
 }
