aa7d342abccb13a238fad92b9da95a9611a934c8
Jenni Tran <jenni90232@gmail.com>
2023-10-24 23:29:42 +0300

    add find product by id method

--- src/app/api/product/[id]/route.js
+++ src/app/api/product/[id]/route.js
@@ -1,4 +1,21 @@
+import prisma from "../../../../../lib/prisma";
+
 export async function GET(request, { params }) {
   const id = params.id;
-  return Response.json({ message: "cakeid: " + id });
+
+  const product = await prisma.product.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!product) {
+    return new Response("Cannot not find product", {
+      status: 404,
+    });
+  }
+
+  console.log("Found product by id ", product);
+
+  return Response.json({ product });
 }
