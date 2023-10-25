3ed4b7a6973ffa74128f3a659407508aa371ef52
Jenni Tran <jenni90232@gmail.com>
2023-10-26 00:07:30 +0300

    add product data

---
+++ src/app/product/[id]/data.js
@@ -0,0 +1,12 @@
+export const cakeSizes = [
+  {
+    label: "6 hlö",
+    value: "small",
+    description: "The second most popular pet in the world",
+  },
+  {
+    label: "12 hlö",
+    value: "normal",
+    description: "The second most popular pet in the world",
+  },
+];
