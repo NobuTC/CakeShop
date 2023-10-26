518f268b1016c197ee2c8b178ac58a021f94d22e
Jenni Tran <jenni90232@gmail.com>
2023-10-27 00:39:48 +0300

    add category page

--- src/app/category/page.js
+++ src/app/category/page.js
@@ -1,3 +1,69 @@
-export default function Search() {
-  return <div>CATEGORYYY!</div>;
+import {
+  Card,
+  CardFooter,
+  Image,
+  Button,
+  CardHeader,
+  CardBody,
+  Textarea,
+  Divider,
+  Link,
+} from "@nextui-org/react";
+
+export default function App() {
+  return (
+    <div>
+      <div className="p-4">
+        <h1 className="text-3xl text-center">Kakku kategoria</h1>
+      </div>
+
+      <div className="live-preview flex h-full w-full not-prose justify-center items-center ">
+        <Card isFooterBlurred radius="lg" className="border-none m-4">
+          <Image
+            isZoomed
+            className="object-cover"
+            height={300}
+            width={400}
+            alt="NextUI Fruit Image with Zoom"
+            src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
+          />
+          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+            <p className="text-tiny text-white/80">Juusto kakut</p>
+            <Button
+              className="text-tiny text-white bg-black/20"
+              variant="flat"
+              color="default"
+              radius="lg"
+              size="sm"
+            >
+              Katso lisää
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card isFooterBlurred radius="lg" className="border-none m-4">
+          <Image
+            isZoomed
+            className="object-cover"
+            height={300}
+            width={400}
+            alt="NextUI Fruit Image with Zoom"
+            src="https://nextui-docs-v2.vercel.app/images/fruit-1.jpeg"
+          />
+          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+            <p className="text-tiny text-white/80">Täyte kakut</p>
+            <Button
+              className="text-tiny text-white bg-black/20"
+              variant="flat"
+              color="default"
+              radius="lg"
+              size="sm"
+            >
+              Katso lisää
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
 }
