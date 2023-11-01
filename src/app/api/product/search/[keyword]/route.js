4e634a3ad343bde8789a303d6abba1b06c353614
Jenni Tran <jenni90232@gmail.com>
2023-11-01 23:06:18 +0200

    add serach product by name

--- src/app/api/product/search/[keyword]/route.js
+++ src/app/api/product/search/[keyword]/route.js
@@ -1,4 +1,20 @@
+import prisma from "../../../../../../lib/prisma";
+
 export async function GET(request, { params }) {
   const keyword = params.keyword;
-  return Response.json({ message: "search: " + keyword });
+
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: keyword,
+        mode: "insensitive",
+      },
+    },
+  });
+
+  if (!products) {
+    return new Response("Cannot find product", { status: 404 });
+  }
+  console.log("Found product with search", products);
+  return Response.json({ products: products, count: products.length });
 }
