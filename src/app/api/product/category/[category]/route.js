2ac4a48dae0de5de5ee8ee9a564c40f8ca265e29
Jenni Tran <jenni90232@gmail.com>
2023-11-01 22:33:31 +0200

    add find products by category API

--- src/app/api/product/category/[category]/route.js
+++ src/app/api/product/category/[category]/route.js
@@ -1,4 +1,22 @@
+import prisma from "../../../../../../lib/prisma";
+
 export async function GET(request, { params }) {
   const category = params.category;
-  return Response.json({ message: "category: " + category });
+  const normalizedCategory = category.toLowerCase();
+
+  const products = await prisma.$queryRaw`
+  SELECT *
+  FROM "Product"
+  WHERE LOWER("category") = ${normalizedCategory}
+`;
+
+  if (!products) {
+    return new Response("Cannot not find product by category", {
+      status: 404,
+    });
+  }
+
+  console.log("Found product by id ", products);
+
+  return Response.json({ products });
 }
