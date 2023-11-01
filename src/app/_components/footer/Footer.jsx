a4764c78574866aa7c3adb6d5bf9b4116f7b8b15
Jenni Tran <jenni90232@gmail.com>
2023-11-01 22:33:11 +0200

    fix footer

--- src/app/_components/footer/Footer.jsx
+++ src/app/_components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="fixed inset-x-0 bottom-0 bg-gray-200 text-center py-2">
+    <footer className="fixed inset-x-0 bottom-0 bg-gray-200 text-center py-2">
       <p>&copy; 2023 Jenni Tran</p>
     </footer>
   );
