5343569f32cc52ea402c9d0eb55cb84832fb8fc6
Jenni Tran <jenni90232@gmail.com>
2023-11-27 22:22:21 +0200

    Revalidate every 1 second

--- src/app/api/order/get/route.js
+++ src/app/api/order/get/route.js
@@ -4,7 +4,17 @@ export async function GET(request) {
   try {
     const orders = await prisma.order.findMany();
 
-    return Response.json({ orders });
+    return Response.json(
+      { orders },
+      {
+        status: 200,
+        headers: {
+          "Cache-Control": "public, s-maxage=1",
+          "CDN-Cache-Control": "public, s-maxage=60",
+          "Vercel-CDN-Cache-Control": "public, s-maxage=3600",
+        },
+      }
+    );
   } catch (error) {
     console.error("Error finding order", error);
     return new Response("Cannot find order", { status: 404 });
